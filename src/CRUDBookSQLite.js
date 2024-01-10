e56d9041d3c5af46947b0ca5b3510e55e3a9cec4
Thegustzazaa <s6506021622167@email.kmutnb.ac.th>
2024-01-10 15:07:02 +0700

    h

--- src/CRUDBookSQLite.js
+++ src/CRUDBookSQLite.js
@@ -15,7 +15,7 @@ app.use(express.json())
 
                     // ไม่สร้างซ่ำ Table ชื่อ books
 db.run(`CREATE TABLE IF NOT EXISTS books( 
-    id INTEAGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     title TEXT,
     author TEXT
 )`)
