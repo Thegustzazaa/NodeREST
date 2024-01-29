0434f3b7c69971e461b0688832635cc2b3a2e401
Thegustzazaa <s6506021622167@email.kmutnb.ac.th>
2024-01-29 11:00:42 +0700

    sdfghj

--- src/CRUDBookSQLite.js
+++ src/CRUDBookSQLite.js
@@ -13,7 +13,7 @@ const db = new sqlite3.Database('./Database/Books.sqlite3')
 
 app.use(express.json())
 
-                    // ไม่สร้างซ่ำ Table ชื่อ books
+// ไม่สร้างซ่ำ Table ชื่อ books
 db.run(`CREATE TABLE IF NOT EXISTS books( 
     id INTEGER PRIMARY KEY,
     title TEXT,
@@ -21,25 +21,25 @@ db.run(`CREATE TABLE IF NOT EXISTS books(
 )`)
 
 
-app.get('/books',(req,res)=>{
-    db.all('SELECT * FROM books', (err,rows) =>{
-        if(err){
+app.get('/books', (req, res) => {
+    db.all('SELECT * FROM books', (err, rows) => {
+        if (err) {
             res.status(500).send(err)
-        }else{
+        } else {
             res.json(rows)
         }
     })
 })
 
 
-app.get('/books/:id',(req,res)=>{
-    db.get('SELECT * FROM books WHERE id = ?' ,req.params.id, (err,row) =>{
-        if(err){
+app.get('/books/:id', (req, res) => {
+    db.get('SELECT * FROM books WHERE id = ?', req.params.id, (err, row) => {
+        if (err) {
             res.status(500).send(err)
-        }else{
-            if(!row){
+        } else {
+            if (!row) {
                 res.status(404).send('Book not fond')
-            }else{
+            } else {
                 res.json(row)
             }
         }
@@ -47,37 +47,37 @@ app.get('/books/:id',(req,res)=>{
 })
 
 
-app.post('/books',(req,res)=>{
+app.post('/books', (req, res) => {
     const book = req.body
-    db.run('INSERT INTO books (title, author) VALUES (?,?)' ,book.title, book.author,function(err){
-        if(err){
+    db.run('INSERT INTO books (title, author) VALUES (?,?)', book.title, book.author, function(err) {
+        if (err) {
             res.status(500).send(err)
-        }else{
-           book.id = this.lastID
-           res.send(book)
+        } else {
+            book.id = this.lastID
+            res.send(book)
         }
     })
 })
 
 
-app.put('/books/:id',(req,res)=>{
+app.put('/books/:id', (req, res) => {
     const book = req.body
-    db.run('UPDATE books SET title = ?, author = ? WHERE = ?' ,book.title, book.author,req.params.id,function(err){
-        if(err){
+    db.run('UPDATE books SET title = ?, author = ? WHERE = ?', book.title, book.author, req.params.id, function(err) {
+        if (err) {
             res.status(500).send(err)
-        }else{
-           res.send(book)
+        } else {
+            res.send(book)
         }
     })
 })
 
 
-app.delete('/books/:id',(req,res)=>{
-    db.run('DELETE FROM books ID = ?' ,req.params.id,function(err){
-        if(err){
+app.delete('/books/:id', (req, res) => {
+    db.run('DELETE FROM books ID = ?', req.params.id, function(err) {
+        if (err) {
             res.status(500).send(err)
-        }else{
-           res.send({})
+        } else {
+            res.send({})
         }
     })
 })
