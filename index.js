6d4723384e06559f0647b0c9f2aad88c7cbdab22
Thegustzazaa <s6506021622167@email.kmutnb.ac.th>
2024-03-04 10:08:29 +0700

    wr

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-
+const cors = require('cors');
 
 mongoose.connect(
     "mongodb://admin:HANyhe20921@node59023-pawarit.proen.app.ruk-com.cloud/", {
@@ -22,7 +22,7 @@ const Book = mongoose.model("Book", {
 
 const app = express()
 app.use(bodyParser.json())
-
+app.use(cors())
 
 app.post('/books', async(req, res) => {
     try {
